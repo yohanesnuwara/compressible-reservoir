c60150021e177f42ef8fd5e0961a4d99b4eeb77e
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2019-11-10 18:16:37 +0700

    Update interpscal.py

--- python/interpscal.py
+++ python/interpscal.py
@@ -7,15 +7,19 @@ from scipy import interpolate
 pressurenew = np.linspace(1, 100, 100)
 
 def interp(pressurenew):
-    pressure_northsea = np.genfromtxt('./SCAL.txt')[:,0]  # in psia
-    pressure_northsea = pressure_northsea * 0.00689  # in MPa
-    compressibility_northsea = np.genfromtxt('./SCAL.txt')[:,1]
-    tck = interpolate.splrep(pressure_northsea, compressibility_northsea, s=0)
+    # SCAL input data
+    pressure_scal = np.genfromtxt('./SCAL.txt')[:,0]  # in psia
+    pressure_scal = pressure_scal * 0.00689  # in MPa
+    K_scal = np.genfromtxt('./SCAL.txt')[:,1]
+    
+    # interpolation
+    tck = interpolate.splrep(pressure_scal, K_scal, s=0)
     compressibility_interp = interpolate.splev(pressurenew, tck, der=0)
-    return(pressure_northsea, compressibility_northsea, compressibility_interp)
+    return(pressure_scal, K_scal, compressibility_interp)
 
-pressure_northsea, compressibility_northsea, c = interp(pressurenew)
-plt.plot(pressure_northsea, compressibility_northsea, 'o', pressurenew, c)
-plt.title('Compressibility vs. Pressure (Fetkovich, 1998)'); plt.xlabel('Pressure (MPa)'); plt.ylabel('Compressibility')
-plt.axis([1, 70, 0, 30])
-plt.show()
+## to plot the interpolated result, activate this section. If not use, inactivate this.
+# pressure_scal, K_scal, c = interp(pressurenew)
+# plt.plot(pressure_scal, K_scal, 'o', pressurenew, c)
+# plt.title('Compressibility vs. Pressure (Fetkovich, 1998)'); plt.xlabel('Pressure (MPa)'); plt.ylabel('Compressibility')
+# plt.axis([1, 70, 0, 30])
+# plt.show()
