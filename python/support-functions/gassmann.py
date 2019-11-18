3091ee45d2f2b8ebcf75e3a28aad8dd469831039
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2019-11-18 10:53:52 +0700

    Create gassmann.py

---
+++ python/support-functions/gassmann.py
@@ -0,0 +1,10 @@
+"Fluid Replacement Modelling, Gassmann (1951)"
+
+def Ks(Kd, Km, Kf, phi):
+    gamma = 1.0 - phi - Kd/Km
+    return Kd + (gamma + phi)**2/(gamma/Km + phi/Kf)
+
+
+def Kd(Ks, Km, Kf, phi):
+    gamma = phi*(Km/Kf - 1.0)
+    return (Ks*(gamma + 1.0) - Km)/(gamma - 1.0 + Ks/Km)
